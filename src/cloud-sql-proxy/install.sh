65e310ba9eb6f61801220b098a72aa7675822d96
Ashwin Madavan <ashwin.madavan@gmail.com>
2025-04-16 21:02:38 -0400

    chore: use wget instead of curl

--- src/cloud-sql-proxy/install.sh
+++ src/cloud-sql-proxy/install.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 if [ "${VERSION:-"latest"}" = "latest" ]; then
 	VERSION="$(
-		curl -s https://api.github.com/repos/GoogleCloudPlatform/cloud-sql-proxy/releases/latest |
+		wget -qO- https://api.github.com/repos/GoogleCloudPlatform/cloud-sql-proxy/releases/latest |
 			grep "tag_name" |
 			cut -d\" -f4 |
 			sed 's/v//'
@@ -23,6 +23,6 @@ aarch64 | armv8*)
 	;;
 esac
 
-curl -sSL "https://storage.googleapis.com/cloud-sql-connectors/cloud-sql-proxy/v${VERSION}/cloud-sql-proxy.linux.${PLATFORM}" -o /usr/local/bin/cloud-sql-proxy
+wget -qO /usr/local/bin/cloud-sql-proxy "https://storage.googleapis.com/cloud-sql-connectors/cloud-sql-proxy/v${VERSION}/cloud-sql-proxy.linux.${PLATFORM}"
 chmod +x /usr/local/bin/cloud-sql-proxy
 ln -sf /usr/local/bin/cloud-sql-proxy /usr/local/bin/cloud_sql_proxy
